d09eaf82d3f06451c77a31af5534330f9ce81ea1
filipe98freitas <filipe98freitas@gmail.com>
2021-08-11 21:17:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,36 @@
 const products = [
   {
     id: 1,
-    name: 'Iphone 11',
+    nome: 'Iphone 11',
     tags: ['smartphone', 'tech', 'apple']
   },
     {
     id: 2,
-    name: 'Macbook Pro',
+    nome: 'Macbook Pro',
     tags: ['laptop', 'tech', 'apple']
   },
     {
     id: 3,
-    name: 'JBL Headphones',
+    nome: 'JBL Headphones',
     tags: ['headphone', 'tech', 'jbl']
   },
     {
     id: 4,
-    name: 'Nike Shorts',
+    nome: 'Nike Shorts',
     tags: ['shorts', 'clothing', 'nike']
   },
 ];
 
 // Declare sua funçao aqui. Nao se esqueça de invocar a função para conseguir testar!
+
+function filterProducts(categoria) {
+  const newArr = []
+  for (let count = 0; count < products.length; count++) {
+    if (products[count].tags.includes(categoria)) {
+      newArr.push(products[count])
+    }
+  }
+    return newArr
+  }
+  const produtoApple = filterProducts('apple');
+  console.log(produtoApple);
